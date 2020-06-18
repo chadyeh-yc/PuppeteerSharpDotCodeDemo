35b0db67b6c45e71254362967a5291686c7a0961
葉永青 Chad Yeh <chad.yeh@miniasp.com>
2020-06-18 16:04:50 +0800

    Add project files.

---
+++ PuppeteerSharpDotCodeDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PuppeteerSharpDotCodeDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
